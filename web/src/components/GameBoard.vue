f5f63dc350aeeff9f06a50350c32cf19f367bcc1
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-08 13:19:58 -0700

    Initialize the components with the Vue base.

---
+++ web/src/components/GameBoard.vue
@@ -0,0 +1,17 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: ``,
+	components: {},
+	computed: {},
+	methods: {},
+}
+</script>
+
+<style>
+@import "css/theme.css";
+@import "css/style.css";
+</style>
