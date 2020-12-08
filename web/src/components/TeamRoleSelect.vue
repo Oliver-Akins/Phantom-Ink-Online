36db1b68a754648bdfff6d10394202a5b541b3ae
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-08 15:53:38 -0700

    Begin work on the role selector for the teams.

--- web/src/components/TeamRoleSelect.vue
+++ web/src/components/TeamRoleSelect.vue
@@ -1,13 +1,42 @@
 <template>
-	<div></div>
+	<div :id="`${team_name}-role-select`">
+		<h2 class="centre">{{ team_name }}</h2>
+		<button
+			@click.stop="joinWriterRole"
+		>
+			{{ writer_name }}
+		</button>
+		<button
+			@click.stop="joinGuesserRole"
+		>
+			{{ guesser_name }}
+		</button>
+	</div>
 </template>
 
 <script>
 export default {
-	name: ``,
+	name: `TeamRoleSelection`,
 	components: {},
+	props: {
+		team_name: {
+			type: String,
+			required: true,
+		},
+		player_name: {
+			type: String,
+			required: true,
+		},
+	},
+	data() {return {
+		writer_name: `Spirit`,
+		guesser_name: `Medium`,
+	}},
 	computed: {},
-	methods: {},
+	methods: {
+		joinWriterRole() {},
+		joinGuesserRole() {},
+	},
 }
 </script>
 
