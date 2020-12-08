f7e71e7cc31c428517887244c19b3dcb171f2ced
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-08 15:02:48 -0700

    Add the first view and the attributions bar.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,14 +1,41 @@
 <template>
-	<div id="app"></div>
+	<div id="app" class="maximize">
+		<CreateJoin v-if="gameState == `login`" />
+		<Attributions />
+	</div>
 </template>
 
 <script>
+import AttributionsBar from "./components/Attributions";
+import CreateJoinGame from "./views/CreateJoin";
+
 export default {
-	name: 'App',
-	components: {}
+	name: `App`,
+	components: {
+		"Attributions": AttributionsBar,
+		"CreateJoin": CreateJoinGame,
+	},
+	computed: {
+		gameState() {
+			return this.$store.state.view;
+		},
+	},
+	methods: {},
 }
 </script>
 
 <style>
+@import "css/theme.css";
+@import "css/style.css";
 
+html, body {
+	background-color: var(--background1);
+	color: var(--light-font-colour);
+	font-family: var(--fonts);
+	overflow: hidden;
+	height: 100vh;
+	width: 100vw;
+	padding: 0;
+	margin: 0;
+}
 </style>
