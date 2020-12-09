3257034b8225ce93762093f9b2ae926cbe213187
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-08 18:09:00 -0700

    Add the moon counts to the data store

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -11,9 +11,21 @@ export default new Vuex.Store({
 		id: null,
 		team_1: {
 			name: `Sun`,
+			moons: {
+				1: 0,	2: 0,
+				3: 0,	4: 0,
+				5: 0,	6: 0,
+				7: 0,	8: 0,
+			},
 		},
 		team_2: {
 			name: `Moon`,
+			moons: {
+				1: 0,	2: 0,
+				3: 0,	4: 0,
+				5: 0,	6: 0,
+				7: 0,	8: 0,
+			},
 		},
 		writer_name: `Spirit`,
 		guesser_name: `Medium`,
