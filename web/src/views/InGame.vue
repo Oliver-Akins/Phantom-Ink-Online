299cc99c8811a5cad5a78d9d5f79b6bb7cabe475
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-10 15:19:11 -0700

    Begin work on the InGame view

--- web/src/views/InGame.vue
+++ web/src/views/InGame.vue
@@ -1,17 +1,32 @@
 <template>
-	<div></div>
+	<div id="GameView" class="maximize">
+		<game-board />
+		<player-hand />
+		<team-reminder />
+		<discard-hand />
+	</div>
 </template>
 
 <script>
+import DiscardHand from "../components/DiscardHandButton";
+import TeamReminder from "../components/TeamReminder";
+import GameBoard from "../components/GameBoard";
+import PlayerHand from "../components/Hand";
+
 export default {
 	name: `InGame`,
-	components: {},
+	components: {
+		"team-reminder": TeamReminder,
+		"discard-hand": DiscardHand,
+		"player-hand": PlayerHand,
+		"game-board": GameBoard,
+	},
 	computed: {},
 	methods: {},
 }
 </script>
 
-<style>
+<style scoped>
 @import "../css/theme.css";
 @import "../css/style.css";
 </style>
