c08e61117e28fb3c70c5b45a236212a3d2e80e34
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-18 18:41:15 -0700

    Begin on the discard hand button

--- web/src/components/DiscardHandButton.vue
+++ web/src/components/DiscardHandButton.vue
@@ -1,17 +1,35 @@
 <template>
-	<div></div>
+	<div id="DiscardHandButton">
+		AH!
+	</div>
 </template>
 
 <script>
 export default {
 	name: `DiscardHand`,
 	components: {},
+	data() {return {
+		confirmVisible: false,
+	}},
 	computed: {},
-	methods: {},
+	methods: {
+		discardHand() {},
+	},
 }
 </script>
 
-<style>
+<style scoped>
 @import "../css/theme.css";
 @import "../css/style.css";
+
+#DiscardHandButton {
+	background-color: var(--background3);
+	border-radius: 100% 0 0 0;
+	height: var(--size);
+	width: var(--size);
+	position: fixed;
+	--size: 120px;
+	bottom: 0;
+	right: 0;
+}
 </style>
