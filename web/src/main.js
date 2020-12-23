31557e5ccfdc99737ac9bb56936950baaa01bfe5
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-23 12:41:09 -0700

    Add Vue-socket.io-extended to the site.

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import io from 'socket.io-client';
+import VueSocketIOExt from 'vue-socket.io-extended';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueSocketIOExt, io(`http://${window.location.hostname}:8081`));
 
 new Vue({
 	store,
 	render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
