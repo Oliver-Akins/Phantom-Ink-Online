708de802a46d23658b7af7c6385f242b25e420ba
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-26 14:45:37 -0700

    Register event listeners

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -2,6 +2,8 @@ import { log } from "./main";
 import { Server, Socket } from "socket.io";
 
 import CreateGame from "./events/CreateGame";
+import UpdatePlayer from "./events/UpdatePlayer";
+import JoinGame from "./events/JoinGame";
 
 
 export default async (conf: config) => {
@@ -13,12 +15,19 @@ export default async (conf: config) => {
 			origin: conf.webserver.hostname,
 			credentials: true,
 		}
-	})
+	});
 
 	io.on(`connection`, (socket: Socket) => {
 		log.debug(`Client connected with ID: ${socket.id}`);
 
+		// Game Management
 		socket.on(`CreateGame`, (data: CreateGame) => CreateGame(io, socket, data));
+		// socket.on(`DeleteGame`, (data: DeleteGame) => DeleteGame(io, socket, data));
+
+
+		// Player Management
+		socket.on(`JoinGame`, (data: JoinGame) => JoinGame(io, socket, data));
+		socket.on(`UpdatePlayer`, (data: UpdatePlayer) => UpdatePlayer(io, socket, data));
 	});
 
 	log.info(`Server started on port ${conf.websocket.port}`);
