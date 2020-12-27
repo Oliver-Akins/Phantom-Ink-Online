e3fbf16e9c10e3578c7754c34440b365b8307ad2
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-12-27 13:29:12 -0700

    Implement the ObjectList event

--- server/src/events/ObjectList.ts
+++ server/src/events/ObjectList.ts
@@ -1,13 +1,27 @@
+import { games, log } from '../main';
 import { Server, Socket } from 'socket.io';
 
 export default (io: Server, socket: Socket, data: ObjectList) => {
 	try {
-		socket.emit(`Error`, {
-			status: 501,
-			message: `ObjectList: Not Implemented Yet`,
-			source: `ObjectList`,
+
+		// Assert game exists
+		if (!games[data.game_code]) {
+			log.debug(`Can't delete game that doesn't exist: ${data.game_code}`);
+			socket.emit(`Error`, {
+				status: 404,
+				message: `Game with code ${data.game_code} could not be found`,
+				source: `ObjectList`
+			});
+			return;
+		};
+		let game = games[data.game_code];
+
+		log.silly(`[${game.id}] Sent client object card.`);
+		socket.emit(`ObjectList`, {
+			objects: game.objects
 		});
-	} catch (err) {
+	}
+	catch (err) {
 		socket.emit(`Error`, {
 			status: 500,
 			message: `${err.name}: ${err.message}`,
