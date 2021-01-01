6cd11b75639b4918f147be3b67e09ee30925d098
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-01-01 16:10:11 -0700

    Add vue-clipboard2 to the system

--- web/src/main.js
+++ web/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import io from 'socket.io-client';
+import clipboard from "vue-clipboard2";
 import VueSocketIOExt from 'vue-socket.io-extended';
 
 Vue.config.productionTip = false;
 
+Vue.use(clipboard);
 Vue.use(VueSocketIOExt, io(`http://${window.location.hostname}:8081`));
 
 new Vue({
