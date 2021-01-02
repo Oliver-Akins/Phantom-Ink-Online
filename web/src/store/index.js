e04a0bca08a6a2dda945c956945c61a1d454390a
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-01-01 23:25:16 -0700

    Add semi-colons.

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 import VuexPersistence from 'vuex-persist';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
