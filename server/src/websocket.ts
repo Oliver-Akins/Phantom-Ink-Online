1512960b2abd0f09306344f9e3511cdeeedc6101
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-01-02 00:34:20 -0700

    Add event registration for DeleteGame and LeaveGame

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -1,9 +1,11 @@
 import { log } from "./main";
 import { Server, Socket } from "socket.io";
 
-import CreateGame from "./events/CreateGame";
-import UpdatePlayer from "./events/UpdatePlayer";
 import JoinGame from "./events/JoinGame";
+import LeaveGame from "./events/LeaveGame";
+import CreateGame from "./events/CreateGame";
+import DeleteGame from "./events/DeleteGame";
+import UpdatePlayer from "./events/UpdatePlayer";
 
 
 export default async (conf: config) => {
@@ -22,12 +24,13 @@ export default async (conf: config) => {
 
 		// Game Management
 		socket.on(`CreateGame`, (data: CreateGame) => CreateGame(io, socket, data));
-		// socket.on(`DeleteGame`, (data: DeleteGame) => DeleteGame(io, socket, data));
+		socket.on(`DeleteGame`, (data: DeleteGame) => DeleteGame(io, socket, data));
 
 
 		// Player Management
 		socket.on(`JoinGame`, (data: JoinGame) => JoinGame(io, socket, data));
 		socket.on(`UpdatePlayer`, (data: UpdatePlayer) => UpdatePlayer(io, socket, data));
+		socket.on(`LeaveGame`, (data: LeaveGame) => LeaveGame(io, socket, data));
 	});
 
 	log.info(`Server started on port ${conf.websocket.port}`);
