2c6d22e92247bf753a24d7c1d3247727b0b1a55c
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-01-05 13:45:22 -0700

    Use a different websocket URI for dev versus prod

--- web/src/main.js
+++ web/src/main.js
@@ -7,8 +7,14 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 
 Vue.config.productionTip = false;
 
+// Get the URI for 
+let websocket_uri = `/`;
+if (process.env.NODE_ENV === `development`) {
+	websocket_uri = `http:${window.location.hostname}:8081`;
+};
+
 Vue.use(clipboard);
-Vue.use(VueSocketIOExt, io(`http://${window.location.hostname}:8081`));
+Vue.use(VueSocketIOExt, io(websocket_uri));
 
 new Vue({
 	store,
