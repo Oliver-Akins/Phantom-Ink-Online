643b6e84794eb3eabb1f6a0b55b5a4d16238992d
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-01-05 13:44:27 -0700

    Add eye counts

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -11,9 +11,9 @@ export default new Vuex.Store({
 			icon: `sun.svg`,
 			eyes: {
 				1: 0,	2: 0,
-				3: 0,	4: 0,
-				5: 0,	6: 0,
-				7: 0,	8: 0,
+				3: 0,	4: 1,
+				5: 0,	6: 1,
+				7: 1,	8: 0,
 			},
 		},
 		team_2: {
@@ -21,8 +21,8 @@ export default new Vuex.Store({
 			icon: `moon.svg`,
 			eyes: {
 				1: 0,	2: 0,
-				3: 0,	4: 0,
-				5: 0,	6: 0,
+				3: 1,	4: 0,
+				5: 1,	6: 1,
 				7: 0,	8: 0,
 			},
 		},
