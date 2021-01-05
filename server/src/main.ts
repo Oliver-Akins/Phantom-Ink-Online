f0f303246590d802109a12b2b968bd1622c95866
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-01-05 16:54:45 -0700

    Add cleanup listeners if they are enabled.

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { readFileSync } from "fs";
 import { Game } from "./objects/Game";
 import startWebsocket from "./websocket";
 import { Validate } from "./utils/validate";
+import { processExit } from "./utils/cleanup";
 
 export const conf: config = toml.parse(readFileSync(`server.toml`, `utf-8`));
 
@@ -18,6 +19,17 @@ export const log: Logger = new Logger({
 	name: `GLOBAL`,
 });
 
+// Ensure the config valid
 if (Validate.config(conf)) {
+
+	// Add event listeners if we want to use the datastore saving game system
+	if (conf.datastores.enabled) {
+
+		// Get games from datastore
+
+		process.on(`uncaughtException`, processExit);
+		process.on(`SIGINT`, processExit);
+	};
+
 	startWebsocket(conf);
 }
