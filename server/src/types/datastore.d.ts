5786ad2742a5763d1fa0f3c2cf88cdd726fbb5a0
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-01-05 16:51:37 -0700

    begin working on the data types needed for the datastore

---
+++ server/src/types/datastore.d.ts
@@ -0,0 +1,30 @@
+interface datastorePlayer {
+	team: team | null;
+	role: role | null;
+	host: boolean;
+	name: string;
+}
+
+type datastoreQuestionCard = string;
+type datastoreObjectCard = string[];
+
+interface datastoreTeam {
+	questions: datastoreQuestionCard[];
+	hand: datastoreQuestionCard[];
+	id: team;
+}
+
+interface datastoreDeck<T> {
+	discard: T[];
+	unknown: T[];
+	deck: T[];
+}
+
+interface datastore {
+	questions: datastoreDeck<question_deck>;
+	objects: datastoreDeck<object_deck>;
+	objectCard: datastoreObjectCard;
+	players: datastorePlayer[];
+	ingame: boolean;
+	object: string;
+}
