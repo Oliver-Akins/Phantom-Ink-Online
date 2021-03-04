50b3efec4c287edeaff184e583a2e2f9a0b4e640
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-03-03 22:22:14 -0700

    Move the prod and dev websocket URI to a config file.

---
+++ web/src/config.js
@@ -0,0 +1,11 @@
+/**
+ * The URI that socket IO tries to connect to for websocket communication when
+ * built for production serving.
+ */
+export const websocket_uri = `/`;
+
+/**
+ * The websocket URI that Socket.IO tries to connect to when live-serving the
+ * site via webpack.
+ */
+export const dev_websocket_uri = 8081;
